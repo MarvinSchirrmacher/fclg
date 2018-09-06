a08cb64930fa628202863f5251d27e355b08f19f
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2018-09-06 23:29:31 +0200

    Refactor theme php module including

--- functions.php
+++ functions.php
@@ -1,15 +1,21 @@
 <?php
-
 define( 'FCO_VERSION', '2.8.4' );
 define( 'FCO_REQUIRED_WP_VERSION', '5.0' );
 define( 'FCO_THEME', __FILE__ );
 define( 'FCO_THEME_DIR', untrailingslashit( dirname( FCO_THEME ) ) );
 define( 'FCO_THEME_TEXTDOMAIN', 'fconline' );
 
-require_once FCO_THEME_DIR . '/includes/setup.php';
-require_once FCO_THEME_DIR . '/includes/modules.php';
-require_once FCO_THEME_DIR . '/includes/widgets.php';
-require_once FCO_THEME_DIR . '/includes/gallery.php';
-require_once FCO_THEME_DIR . '/includes/shortcodes.php';
+// the order matters
+$paths = array(
+    '/includes/services.php',
+    '/includes/setup.php',
+    '/includes/modules.php',
+    '/includes/widgets.php',
+    '/includes/gallery.php',
+    '/includes/shortcodes.php'
+);
 
+foreach ($paths as $path) {
+    require_once FCO_THEME_DIR . $path;
+}
 ?>
