409ababaa2469a02e1176415e80c2fcedcc77d8d
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2019-03-29 08:50:35 +0100

    download button: refactor and display image if present

--- content-download.php
+++ content-download.php
@@ -1,10 +1,30 @@
 <?php
-// Shows title and file information.
-if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
-$file_type = end(explode('.', $dlm_download->get_the_filename()));
+/**
+ * Theme-like button with title, file type and file size.
+ */
+
+if (download_is_accessed_directly()) { exit; } // is_accessed_directly
+
+$type = end(explode('.', $dlm_download->get_the_filename()));
+$size = $dlm_download->get_the_filesize();
+$title = $dlm_download->get_the_title();
+$version = $dlm_download->has_version_number()
+	? sprintf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_the_version_number())
+	: '';
+$link = $dlm_download->get_the_download_link();
+$image = $dlm_download->get_the_image();
+$meta = 'data-filetype="'.$type.' / '.$size.'" title="'.$title.' ('.$version . ')" href="'.$link.'" rel="nofollow"';
 ?>
-<a class="download-link button" data-filetype="<?php echo $file_type; ?> / <?php $dlm_download->the_filesize(); ?>" title="<?php if ( $dlm_download->has_version_number() ) {
-	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_the_version_number() );
-} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php $dlm_download->the_title(); ?>
+
+<?php if (download_has_image($image)): ?>
+<a class="download-link post-preview" <?php echo $meta; ?>>
+	<div class="post-thumbnail"><?php echo $image; ?></div>
+	<!-- <figcaption class="post-meta">
+		<span class="post-title"><?php // echo $title; ?></span>
+	</figcaption> -->
 </a>
+<?php else: ?>
+<a class="download-link button" <?php echo $meta; ?>>
+	<?php echo $title; ?>
+</a>
+<?php endif; ?>
