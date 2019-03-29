b78946aeaa44ba69bc56bd7388be50ba32e147b2
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2019-03-29 08:47:48 +0100

    compass: add style parsing comments

--- config.rb
+++ config.rb
@@ -9,14 +9,16 @@ images_dir = "images"
 javascripts_dir = "scripts"
 fonts_dir = "fonts"
 
+# output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
+# To disable debugging comments that display the
+# original location of your selectors. Uncomment:
 line_comments = false
 color_output = false
-
 preferred_syntax = :scss
 
 require 'fileutils'
