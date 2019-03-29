0d7e044008f5a4ed21990c249bdebf2669b129fd
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2019-03-29 08:54:11 +0100

    header: outsource navigation assembly to function

--- header.php
+++ header.php
@@ -14,18 +14,9 @@
 <a name="top"></a>
 
 <body <?php body_class(); ?>>
-
 	<a href="#top" id="back-to-top" class="hidden"></a>
-
 	<section id="page">
-	<?php $nav_menu_id = 'primary'; ?>
-	<?php $has_nav_menu = has_nav_menu($nav_menu_id); ?>
-	<?php if($has_nav_menu): ?>
-		<div id="navigation-toggle" class="toggle"></div>
-	<?php endif;?>
-
 		<header id="page-header" role="banner">
-
 			<div id="top-strip" class="strip">
 				<div id="page-title-wrapper" class="inside">
 					<?php include get_template_directory() . "/images/title.svg"; ?>
@@ -35,25 +26,6 @@
 			<div id="middle-strip" class="strip"></div>
 			<div id="bottom-strip" class="strip"></div>
 			<a id="site-logo" href="<?php bloginfo('url'); ?>"><span></span></a>
-
 		</header>
-
-		<?php
-		if ($has_nav_menu) {
-			$nav_menu_parameters = array(
-				'theme_location'  => $nav_menu_id,
-				'container'       => 'div',
-				'container_class' => 'wrapper strip hidden',
-				'container_id'    => 'navigation-wrapper',
-				'menu_class'      => 'menu inside grid',
-				'menu_id'         => 'navigation-menu',
-				'echo'            => false,
-				'fallback_cb'     => 'wp_page_menu',
-				'items_wrap'      => '<nav id="%1$s" class="%2$s">%3$s</nav>',
-				'depth'           => 0
-			);
-			echo preg_replace('/<ul\s+class=".*?sub-menu.*?">/', '<div id="sub-menu-toggle" class="toggle sub-menu-toggle"></div><ul id="sub-menu-wrapper" class="wrapper menu sub-menu hidden">', wp_nav_menu( $nav_menu_parameters ));
-		}
-		?>
-
+		<?php echo FcOnline::assembleNavMenu('primary'); ?>
 		<section id="page-main" class="inside grid">
