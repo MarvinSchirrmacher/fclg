060a9770098872c4815cf845f99828b35a26c210
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2019-10-26 00:07:27 +0200

    team: update fussballde widget version

--- scripts/fussballde-widget.js
+++ scripts/fussballde-widget.js
@@ -2,56 +2,53 @@
  * fussball.de widgetAPI
  */
 
-var egmWidgetUrl = '//www.fussball.de/widget2';
+var egmWidget2 = {};
 
-var referer = '';
-if(location.host){
-	referer = encodeURIComponent(location.host);
-}else{
-	referer = 'unknown';
+egmWidget2.url = '//www.fussball.de/widget2';
+
+egmWidget2.referer = '';
+if (location.host) {
+	egmWidget2.referer = encodeURIComponent(location.host);
+} else {
+	egmWidget2.referer = 'unknown';
 }
 
-fussballdeWidgetAPI = function() {
-	var D = new Object();
-	var C = new Object();
+function createIFrame(parentId, src) {
+	var parent = document.getElementById(parentId);
+	var iframe = document.createElement('iframe');
+	iframe.frameBorder = 0;
+	iframe.setAttribute("src", src);
+	iframe.setAttribute("scrolling", "no");
+	iframe.setAttribute("width", "500px");
+	iframe.setAttribute("height", "500px");
+	iframe.setAttribute("style", "border: 1px solid #CECECE;");
+	parent.innerHTML = "";
+	parent.appendChild(iframe);
+}
 
-	D.showWidget = function(E, K) {
-		if (K != undefined && K != null && K != "" && E != undefined && E != null && E != "") {
+fussballdeWidgetAPI = function () {
+	var D = {};
+
+	D.showWidget = function (E, K) {
+		if (K !== undefined && K !== null && K !== "" && E !== undefined && E !== null && E !== "") {
 			if (document.getElementById(E)) {
-				if (K != "") {
-					var src = egmWidgetUrl + "/-"
-					    + "/schluessel/" + K
-						+ "/target/" + E
-						+ "/caller/" + referer;
+				if (K !== "") {
+					var src = egmWidget2.url + "/-" + "/schluessel/" + K + "/target/" + E + "/caller/" + egmWidget2.referer;
 					createIFrame(E, src);
 				}
 			} else {
-				alert("Der angegebene DIV mit der ID " + E
-						+ " zur Ausgabe existiert nicht.")
 			}
 		}
 	};
 
-	window.addEventListener("message", receiveMessage, false);
-	function receiveMessage(event)
-	{
-	  if (event.data.type === 'setHeight'){
-	    document.querySelectorAll('#' + event.data.container + ' iframe')[0].setAttribute('height', event.data.value + 'px');
-	  }
-	}
-	return D
+	window.addEventListener("message", function (event) {
+		if (event.data.type === 'setHeight') {
+			document.querySelectorAll('#' + event.data.container + ' iframe')[0].setAttribute('height', event.data.value + 'px');
+		}
+		if (event.data.type === 'setWidth') {
+			document.querySelectorAll('#' + event.data.container + ' iframe')[0].setAttribute('width', event.data.value + 'px');
+		}
+	}, false);
+
+	return D;
 };
-
-
-function createIFrame(parentId, src){
-	var parent = document.getElementById(parentId);
-	var iframe = document.createElement('iframe');
-	iframe.frameBorder=0;
-	iframe.setAttribute("src", src);
-	iframe.setAttribute("scrolling", "no");
-	iframe.setAttribute("width", "550px");
-	iframe.setAttribute("height", "500px");
-	iframe.setAttribute("style", "border: 1px solid #CECECE;");
-	parent.innerHTML="";
-	parent.appendChild(iframe);
-}
