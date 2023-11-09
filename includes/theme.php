910c2e0b3ffe890a31ee42c60b46ca6d86cae90d
Marvin Schirrmacher <marvin.schirrmacher@miele.com>
2023-11-09 21:08:56 +0100

    remove possibility to log in via ui
    The usage of the theme does not make it necessary to enable the user to
    log in to the backend. Therefore the log in button next to the search
    field can be removed.

--- includes/theme.php
+++ includes/theme.php
@@ -4,25 +4,24 @@ class FcOnline {
 		if (is_user_logged_in()) {
 			return wp_logout_url(home_url());
 		}
-		
+
 		$redirectUrl = get_option('fco_login_redirect_url');
 		$redirectUrl = filter_var($redirectUrl, FILTER_SANITIZE_URL);
 		$redirectUrl = filter_var($redirectUrl, FILTER_VALIDATE_URL);
 		return wp_login_url($redirectUrl ? $redirectUrl : home_url());
 	}
-	
+
 	public static function assembleNavMenu($menu_slug) {
 		if (!has_nav_menu($menu_slug)) {
 			return '';
 		}
 
 		$search_toggle = '<div id="srch-tog" class="tog search-form-tog round"></div>';
-		$login_button = sprintf('<a href="%s" id="login" class="button round"></a>', FcOnline::getLoginUrl());
 		$search_form = get_search_form(false);
 		$search_form = preg_replace('/<\/?p>/m', '', $search_form);
 		$search_form = preg_replace('/id="search-form"/m',
 			'id="srch-wrp" class="wrp hidden"', $search_form);
-		$pre_nav_menu = $search_toggle.$search_form.$login_button;
+		$pre_nav_menu = $search_toggle.$search_form;
 
 		$nav_menu = wp_nav_menu(array(
 			'theme_location'  => $menu_slug,
@@ -43,7 +42,7 @@ class FcOnline {
 			'<div id="sub-tog" class="tog sub-tog icn round"></div>'.
 			'<ul id="sub-wrp" class="wrp menu sub-menu flex hidden">';
 		$nav_menu = preg_replace($sub_menu_pattern, $sub_menu, $nav_menu);
-		
+
 		$nav_menu_pattern = '/<nav /';
 		$nav_menu = preg_replace($nav_menu_pattern, $pre_nav_menu.'<nav ', $nav_menu);
 		$nav_menu_toggle = '<div id="nav-tog" class="tog icn round grey shaded"></div>';
