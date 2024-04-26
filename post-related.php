75e7eaa584e928e851f68911cd20267f1f45888b
Marvin Schirrmacher <marvin.schirrmacher@icloud.com>
2024-04-27 00:26:20 +0200

    show grey thumbnail for related posts

--- post-related.php
+++ post-related.php
@@ -1,7 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-	<h6 class="post-title sitemap-title"><a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a></h6>
-
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+	
+	<div class="post-header">
+		<h6 class="post-title sitemap-title"><a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a></h6>
+	</div>
 	<div class="post-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .post-summary -->
